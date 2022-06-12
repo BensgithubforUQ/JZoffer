9fd62ebe499a3428b495a2c95c64b9a84c67197c
BensgithubforUQ <393114076@qq.com>
2022-06-12 21:38:56 +0800

    回溯法

--- JZ38.cpp
+++ JZ38.cpp
@@ -2,6 +2,16 @@
 
 //输入一个长度为 n 字符串，打印出该字符串中字符的所有排列，你可以以任意顺序返回这个字符串数组。
 //例如输入字符串ABC, 则输出由字符A, B, C所能排列出来的所有字符串ABC, ACB, BAC, BCA, CBA和CAB。
+
+//回溯法是五大经典算法之一，不过个人感觉其算是五大算法中的最简单的一种了，
+//回溯法解决主要解决搜索问题，即枚举出所有的情况。其有固定的模板，而且常见题型固定，
+//只要掌握了这几种基本题型就可以做出大多数回溯法的题。回溯法主要找出这三个问题就可解决：
+//
+//路径：也就是已经做出的选择。
+//选择列表：也就是你当前可以做的选择。
+//结束条件：也就是到达决策树底层，无法再做选择的条件。
+//代码方面直接套用如下模板即可
+
 vector<string> Solution4::Permutation(string str) {
 	vector<string> res;
 	set<string> res_s;
@@ -30,10 +40,10 @@ void Solution4::permutation(set<string>& ans, string str, int index) {
 		swap(str[i], str[index]); //交换 查询
 		cout<<str<<endl;
 		permutation(ans, str, index + 1);
-		swap(str[i], str[index]); //交换回来  回溯
+		swap(str[i], str[index]); //交换回来  回溯   
 		cout << "回转 index: " << index << " i: " << i << " " << str << endl;
 	}
-	cout << "end!!!!" << endl;
+	cout << "i = 3 end!!!!" << "回转 index: " << index << endl;
 }
 int main() {
 	string s = "abc";
