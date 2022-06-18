c742860864fef68c3d1fe27f6589523baca15915
BensgithubforUQ <393114076@qq.com>
2022-06-18 09:16:45 +0800

    暴力51，测试超时

---
+++ JZ51.cpp
@@ -0,0 +1,28 @@
+#include <set>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int InversePairs(vector<int> data) {
+        int n = 0;
+        set<int> s_data;
+        int size = 0;
+        for (int i = 0; i < data.size(); i++) {
+            s_data.insert(data[i]);
+            size++;
+            int index = 1;
+            for (set<int>::iterator j = s_data.begin(); j != s_data.end(); j++, index++)
+                if (data[i] == *j) break;
+            if (size > index) n += size - index;
+
+        }
+
+        return n % 1000000007;
+    }//±©Á¦½â·¨
+
+    int recursion() {
+
+    }
+};
