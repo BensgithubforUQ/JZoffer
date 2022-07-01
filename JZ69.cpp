cdd866734ffae743a9f0de4634cf7d486627af43
BensgithubforUQ <393114076@qq.com>
2022-07-01 22:19:57 +0800

    递归，动态规划，跳格子

---
+++ JZ69.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jumpFloor(int number) {//动态规划问题
+        if (number == 0) return 1; //因为题目中给的number不可能等于0，因此不需要判断了
+        if (number == 1) return 1;
+        if (number >= 2) {
+            return jumpFloor(number - 1) + jumpFloor(number - 2);
+        }
+        return 0;
+    }
+};
+//跳台阶问题，无非是两种跳法，一种是一次跳一格子，一种是一次跳俩格子
+//简化问题，就是当台阶大于1的时候，就可能-1或者-2，当台阶==2的时候-1==1，-2==0
+//因此number==0就默认是-2的情况，而==1就是-1的情况，都是++1
+//而==1的情况直接返回1，因为只有一种跳的方式
+//而number>2的时候，就需要递归，如果计算完了仍然>2,则继续递归。
