3227a05f5809515fa1745a54813a5cb1ef563c34
BensgithubforUQ <393114076@qq.com>
2022-07-02 11:30:40 +0800

    动态规划

--- JZ69.cpp
+++ JZ69.cpp
@@ -14,3 +14,16 @@ public:
 //因此number==0就默认是-2的情况，而==1就是-1的情况，都是++1
 //而==1的情况直接返回1，因为只有一种跳的方式
 //而number>2的时候，就需要递归，如果计算完了仍然>2,则继续递归。
+class Solution {
+public:
+    int jumpFloorII(int number) { //跳台阶扩展，这次需要把之前的每个数值的结果都加到一起。
+        //因为每个结构都可以再跳一步直接倒终点，形成新的解决方案。
+        if (number == 0) return 1;
+        if (number == 1) return 1;
+        if (number == 2) return 2;
+        int res = 0;
+        for (; number > 0; number--)
+            res += jumpFloorII(number - 1);
+        return  res;
+    }
+};
