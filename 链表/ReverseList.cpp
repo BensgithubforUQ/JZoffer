19f37995dd48f02f36cc243bc6cb44900172c581
BensgithubforUQ <393114076@qq.com>
2022-07-11 20:55:32 +0800

    反转链表三连

--- 链表/ReverseList.cpp
+++ 链表/ReverseList.cpp
@@ -55,4 +55,45 @@ public:
         start->next = cur;
         return pre->next;
     }
+
+
+    //反转链表，模板3，更高难度的反转。
+    ListNode* reverse(ListNode* pre, int k) { //k段链表的颠倒。pre-> k->next 变成 pre->颠倒后的k->next; 
+        if (pre == nullptr || pre->next == nullptr || k == 1) return pre;
+        ListNode* start = pre; //起点
+        ListNode* head = pre->next;
+        ListNode* cur = head; //当前
+        ListNode* nex = head->next; //下一个
+        ListNode* fast = pre; //双指针
+        for (int i = 0; i < k; i++) {
+            if (fast->next != nullptr)
+                fast = fast->next;//快指针先跑k步，判断是否满足k的倍数
+            else
+                return nullptr; //如果不满足k个，则返回null，不进行颠倒
+        }
+        while (k > 1) { //否则开始颠倒
+            head = nex; //head挪到下一个
+            nex = head->next; //nex挪到下一个
+            head->next = cur; //head的next指向前一个
+            cur = head; //cur记录新的
+            k--; //k降低一个
+        }
+        start->next->next = nex; //原来的起点的下一个指向原本链表段的最后一个元素的下一个
+        ListNode* temp = start->next; //原来的起点
+        start->next = cur;
+        if (temp->next) return temp; //这一步很重要的，，不然如果k == size，会导致无限循环。。
+        else return nullptr; //如果颠倒完之后，发现新的pre的下一个数是空的，那说明刚好完成任务，返回null
+        //如果不加这一步，会导致刚好的情况无限循环、
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // write code here
+        if (head == nullptr || head->next == nullptr || k == 1) return head;
+        ListNode* pre = new ListNode(0);
+        pre->next = head;
+        ListNode* res = pre;
+        while (res) {
+            res = reverse(res, k);
+        }
+        return pre->next;
+    }
 };
