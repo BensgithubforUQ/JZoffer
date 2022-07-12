dae201674ee9f865880fa89bd7d52b4e2be9462f
BensgithubforUQ <393114076@qq.com>
2022-07-13 00:49:54 +0800

    fix some bugs

--- 链表/KthNodeInList.cpp
+++ 链表/KthNodeInList.cpp
@@ -46,4 +46,21 @@ public:
         slow->next = slow->next->next; //删除倒数第k个结点
         return head;
     }
+
+    ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2) {
+        //两个链表共用了结点，那就分别用俩指针开始遍历两个链表
+        //遍历完了开始遍历另一个链表
+        //这些操作同步进行，找到相同的结点就是第一个结点
+        if (pHead1 == nullptr || pHead2 == nullptr) return NULL;
+        ListNode* p1 = pHead1;
+        ListNode* p2 = pHead2;
+        while (p1 != p2) {//代码逻辑注意顺序，这个地方必须判断是否相等，是因为如果第一个就相等，不判断就会略过
+            if (p1)p1 = p1->next;//不一样，就往后挪
+            if (p2)p2 = p2->next;
+            if (p1 == p2 && p1 == nullptr) return NULL;//挪完了如果一样了，但是空，那说明就没有一样的
+            if (p1 == nullptr) p1 = pHead2; //否则，挪到头了，换另外一个
+            if (p2 == nullptr) p2 = pHead1;
+        }
+        return p1;//最后就该是正确的。
+    }
 };
