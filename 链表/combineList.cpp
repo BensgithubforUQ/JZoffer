9d4440ebafc07ea40741960934727cd60031b2dc
BensgithubforUQ <393114076@qq.com>
2022-07-12 20:30:26 +0800

    分治法合并k个链表

--- 链表/combineList.cpp
+++ 链表/combineList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct ListNode {
@@ -28,4 +29,24 @@ public:
         if (pHead2) head->next = pHead2; //没空的那个，后面剩下的都是大的，那就直接尾插
         return pre->next; //头结点的下一个就是返回值、
     }
+
+
+    //利用这个合并功能，配合分治法，就能针对n个有序链表进行合并
+    ListNode* mergeSort(vector<ListNode*>& lists, int begin_id, int end_id) {//递归的归并排序主体，用来分治
+        if (begin_id == end_id) return lists[begin_id];
+
+        int mid_id = (end_id - begin_id) / 2 + begin_id;
+        ListNode* temp1 = mergeSort(lists, begin_id, mid_id);
+        ListNode* temp2 = mergeSort(lists, mid_id + 1, end_id);
+        ListNode* res = Merge(temp1, temp2); //用到两两合并的merge
+        return res;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {//排序的主函数，用来进入递归和判断某些特殊情况
+        ///ListNode * head = new ListNode(INT_MIN);
+        if (lists.size() == 1) return lists[0];
+        if (lists.size() == 0) return NULL;
+        return mergeSort(lists, 0, lists.size() - 1);
+    }
+
 };
