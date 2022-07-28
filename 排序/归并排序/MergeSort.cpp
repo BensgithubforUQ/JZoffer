4470bc17e4a381c2c005a33cdca63d298ea4330b
BensgithubforUQ <393114076@qq.com>
2022-07-28 23:47:48 +0800

    update

--- 排序/归并排序/MergeSort.cpp
+++ 排序/归并排序/MergeSort.cpp
@@ -36,11 +36,57 @@ void merge(vector<int>& arr, int begin_index, int mid_index, int end_index) {
 		arr[j] = temp[i];
 }
 
+//int main() {
+//	vector<int> arr = { 6,5,4,8,9,7,10,2,1,3,12,14 }; //原教旨归并排序
+//	mergeSort(arr, 0, arr.size() - 1);
+//	for (auto i : arr) {
+//		cout << i <<" ";
+//	}
+//	return 0;
+//}
+
+char low2up(char a) {
+    if (a >= 'A' && a <= 'Z')
+        return a - 'A' + 'a';
+    return a;
+}
+
+void merge1(vector<char>& v, int left, int mid, int right) {
+    vector<char> temp;
+    int i1 = left;
+    int i2 = mid + 1;
+    while (i1 <= mid && i2 <= right) {
+        low2up(v[i1]) <= low2up(v[i2]) ? (temp.push_back(v[i1++])) : (temp.push_back(v[i2++]));
+    }
+    while (i1 <= mid) temp.push_back(v[i1++]);
+    while (i2 <= right) temp.push_back(v[i2++]);
+    for (int i = 0, j = left; i < temp.size(); i++, j++) v[j] = temp[i];
+}
+
+void mergeSort1(vector<char>& v, int begin, int end) {
+    if (begin == end) return;
+    int mid = (end + begin) / 2;
+    mergeSort1(v, begin, mid);
+    mergeSort1(v, mid + 1, end);
+    merge1(v, begin, mid, end);
+}
+
 int main() {
-	vector<int> arr = { 6,5,4,8,9,7,10,2,1,3,12,14 }; //原教旨归并排序
-	mergeSort(arr, 0, arr.size() - 1);
-	for (auto i : arr) {
-		cout << i <<" ";
-	}
-	return 0;
+    char c;
+    vector<char> v;
+    vector<char> res;
+    int pos = 0;
+    while ((c = getchar()) != '\n') {
+        if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')) v.push_back(c);
+        res.push_back(c);
+    }
+    mergeSort1(v, 0, v.size()-1);
+    for (int i = 0, j = 0; i < res.size() && j < v.size(); i++) {
+        if ((res[i] <= 'z' && res[i] >= 'a') || (res[i] <= 'Z' && res[i] >= 'A')) {
+            res[i] = v[j];
+            j++;
+        }
+    }
+    for (auto i : res) cout << i;
+    return 0;
 }
